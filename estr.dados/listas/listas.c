3c8a3eb4d1663951d0cc93cd11989858104ca0ba
Edson Augusto <edsonaugustotl@gmail.com>
2023-09-25 14:19:59 -0300

    working with linked list on C

---
+++ estr.dados/listas/listas.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+	int value;
+	struct node *next;
+}Node;
+
+typedef struct
+{
+	Node *start;
+	int size;
+}List;
+
+void insert_node(List *l, int n)
+{
+	Node *new = (Node*)malloc(sizeof(Node));
+	new->value = n;
+	new->next = l->start;
+	l->start = new; 
+	l->size++;
+
+}
+
+void print_list(List *l)
+{
+	Node *start = l->start;
+	while(start != NULL)
+	{
+		printf("%d ", start->value);
+		start = start->next;
+	}
+	printf("\n\n");
+}
+
+
+
+void main()
+{
+	List *l;
+	int option, value;
+	l = (List*) malloc(sizeof(List));
+	l->start = NULL;
+	l->size = 0;
+	scanf("%d", &value);
+	
+	insert_node(l,value);
+	insert_node(l,value);
+	insert_node(l,value);
+	print_list(l);
+}
